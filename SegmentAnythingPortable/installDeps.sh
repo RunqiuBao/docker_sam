dd2cd4833afb431772d6171ca535fa7548812a52
runqiu.bao <runqiu.bao@mujin.co.jp>
2023-11-10 15:49:08 +0900

    add deps install; add bash tool

--- SegmentAnythingPortable/installDeps.sh
+++ SegmentAnythingPortable/installDeps.sh
@@ -3,4 +3,5 @@ pip install -q git+https://github.com/huggingface/transformers.git datasets
 pip install -q monai
 #Patchify to divide large images into smaller patches for training. (Not necessary for smaller images)
 pip install patchify
-pip install opencv-python 
+pip install opencv-python
+pip install -U matplotlib
