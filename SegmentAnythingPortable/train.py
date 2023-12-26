6c666aff3ff83229776c0182f877abfbde2e04a0
runqiu.bao <runqiu.bao@mujin.co.jp>
2023-12-26 14:34:23 +0900

    resize dataset images for training sam mask decoder

--- SegmentAnythingPortable/train.py
+++ SegmentAnythingPortable/train.py
@@ -9,12 +9,17 @@ from tqdm import tqdm
 from statistics import mean
 import torch
 from torch.nn.functional import threshold, normalize
+import torch.nn.functional as F
 
 from transformers import SamProcessor
 
+import sys
+sys.path.append('/root/data2/ftsam/')
+from ftsam import FtSamDataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("nielsr/breast-cancer", split="train")
+#    dataset = load_dataset("nielsr/breast-cancer", split="train")
+    dataset = FtSamDataset("train")
     processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
     train_dataset = SAMDataset(dataset=dataset, processor=processor)
     train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
@@ -27,7 +32,6 @@ if __name__ == "__main__":
 
     # Note: Hyperparameter tuning could improve performance here
     optimizer = Adam(model.mask_decoder.parameters(), lr=1e-5, weight_decay=0)
-
     seg_loss = monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
 
     num_epochs = 100
@@ -48,6 +52,9 @@ if __name__ == "__main__":
           # compute loss
           predicted_masks = outputs.pred_masks.squeeze(1)
           ground_truth_masks = batch["ground_truth_mask"].float().to(device)
+          gtHeight, gtWidth = ground_truth_masks.shape[-2:]
+          predicted_masks = F.interpolate(predicted_masks, size=(gtHeight, gtWidth))
+#          from IPython import embed; embed()
           loss = seg_loss(predicted_masks, ground_truth_masks.unsqueeze(1))
     
           # backward pass (compute gradients of parameters w.r.t. loss)
