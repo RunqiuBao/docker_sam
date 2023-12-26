4adde3466fbdfd1ce5e8af15d978678bbe051a62
runqiu.bao <runqiu.bao@mujin.co.jp>
2023-12-26 14:33:50 +0900

    mount dataset folder; print when pip installing transformers; add kk alias

--- SegmentAnythingPortable/installDeps.sh
+++ SegmentAnythingPortable/installDeps.sh
@@ -1,7 +1,9 @@
 # Install the required libraries
-pip install -q git+https://github.com/huggingface/transformers.git datasets
+pip install -v -q git+https://github.com/huggingface/transformers.git datasets
 pip install -q monai
 #Patchify to divide large images into smaller patches for training. (Not necessary for smaller images)
 pip install patchify
 pip install opencv-python
 pip install -U matplotlib
+
+echo "alias kk='kill -9 %'" >> ~/.bashrc
--- docker/run.sh
+++ docker/run.sh
@@ -9,6 +9,7 @@ docker run -it \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/usr/local:/usr/local" \
     --volume="$(pwd)/../SegmentAnythingPortable:/root/code/SegmentAnythingPortable:rw" \
+    --volume="/home/mujin/tmptmp/finetuneSam:/root/data2:rw" \
     --volume="$(pwd)/../data:/root/data:rw" \
     --name=sampytorch \
     sampytorch \
